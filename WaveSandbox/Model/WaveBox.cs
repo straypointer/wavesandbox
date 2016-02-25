0d5e1675899d7e10a9cfbc2d027370c0c71122b6
Matt Galligan <matt.galligan@buildingstart.com>
2016-02-25 14:14:00 -0600

    viewmodel->model plumbing

---
+++ WaveSandbox/Model/WaveBox.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaveSandbox.Model {
+	
+	public class WaveBox {
+
+		public WaveBox() {
+
+		}
+
+		public string File1Path { get; set; }
+		public string File2Path { get; set; }
+		public string OutputPath { get; set; }
+
+		public bool CombineFiles() {
+			return true;
+		}
+
+
+	}
+}
--- WaveSandbox/ViewModel/MainViewModel.cs
+++ WaveSandbox/ViewModel/MainViewModel.cs
@@ -2,41 +2,44 @@ using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using System.Windows.Input;
 using WaveSandbox.Extensions;
+using WaveSandbox.Model;
 
-namespace WaveSandbox.ViewModel
-{
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
-    public class MainViewModel : ViewModelBase
-    {
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
-        public MainViewModel()
-        {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+namespace WaveSandbox.ViewModel {
+	/// <summary>
+	/// This class contains properties that the main View can data bind to.
+	/// <para>
+	/// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+	/// </para>
+	/// <para>
+	/// You can also use Blend to data bind with the tool's support.
+	/// </para>
+	/// <para>
+	/// See http://www.galasoft.ch/mvvm
+	/// </para>
+	/// </summary>
+	public class MainViewModel : ViewModelBase {
+		/// <summary>
+		/// Initializes a new instance of the MainViewModel class.
+		/// </summary>
+
+		WaveBox _sandbox = null;
+
+		public MainViewModel() {
+			////if (IsInDesignMode)
+			////{
+			////    // Code runs in Blend --> create design time data.
+			////}
+			////else
+			////{
+			////    // Code runs "for real"
+			////}
+
+			_sandbox = new WaveBox();
 
 			this.SelectFile1Command = new RelayCommand<object>(p => SelectFile1(p));
 			this.SelectFile2Command = new RelayCommand<object>(p => SelectFile2(p));
 			this.CombineFileCommand = new RelayCommand<object>(p => CombineFiles(p));
-        }
+		}
 
 		public ICommand SelectFile1Command {
 			get;
@@ -53,17 +56,33 @@ namespace WaveSandbox.ViewModel
 			private set;
 		}
 
+		public bool IsCombineAvailable {
+			get { return !string.IsNullOrWhiteSpace(_sandbox.File1Path) && !string.IsNullOrWhiteSpace(_sandbox.File2Path); }
+		}
+
+		private const string WAVE_FILTER = "Wave files (*.wav)|*.wav";
+		private void PostViewModelUpdates() {
+			this.RaisePropertyChanged("IsCombineAvailable");
+		}
+
 		public void SelectFile1(object o) {
-			UIServices.ShowMessageBox("Coming Soon!", "Foo");
+			_sandbox.File1Path = UIServices.SelectFileWithWin32OpenDialog(WAVE_FILTER);
+			this.PostViewModelUpdates();
 		}
 
 		public void SelectFile2(object o) {
-
+			_sandbox.File2Path = UIServices.SelectFileWithWin32OpenDialog(WAVE_FILTER);
+			this.PostViewModelUpdates();
 		}
 
 		public void CombineFiles(object o) {
-
+			var result = _sandbox.CombineFiles();
+			if (result) {
+				UIServices.ShowMessageBox("success!");
+			} else {
+				UIServices.ShowMessageBox("fail!");
+			}
 		}
 
-    }
+	}
 }
