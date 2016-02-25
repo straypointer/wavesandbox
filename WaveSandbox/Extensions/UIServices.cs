3bbd6f2980225a224235884c9d604a84c93e3111
Matt Galligan <matt.galligan@buildingstart.com>
2016-02-25 14:01:41 -0600

    hookup viewmodel.

---
+++ WaveSandbox/Extensions/UIServices.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+using Microsoft.Win32;
+
+namespace WaveSandbox.Extensions {
+	
+	public class UIServices {
+
+		/// <summary>
+		///   A value indicating whether the UI is currently busy
+		/// </summary>
+		private static bool IsBusy;
+
+		/// <summary>
+		/// Sets the busystate as busy.
+		/// </summary>
+		public static void SetBusyState() {
+			SetBusyState(true);
+		}
+
+		/// <summary>
+		/// Sets the busystate to busy or not busy.
+		/// </summary>
+		/// <param name="busy">if set to <c>true</c> the application is now busy.</param>
+		private static void SetBusyState(bool busy) {
+			if (busy != IsBusy) {
+				IsBusy = busy;
+				Mouse.OverrideCursor = busy ? Cursors.Wait : null;
+
+				if (IsBusy) {
+					new DispatcherTimer(TimeSpan.FromSeconds(0), DispatcherPriority.ApplicationIdle, dispatcherTimer_Tick, Application.Current.Dispatcher);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Handles the Tick event of the dispatcherTimer control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+		private static void dispatcherTimer_Tick(object sender, EventArgs e) {
+			var dispatcherTimer = sender as DispatcherTimer;
+			if (dispatcherTimer != null) {
+				SetBusyState(false);
+				dispatcherTimer.Stop();
+			}
+		}
+
+		private const string STOCK_TITLE = "Message";
+
+		public static void ShowMessageBox(string msg, string title = STOCK_TITLE) {
+			MessageBox.Show(msg, title);
+		}
+
+		public static bool ShowMessageBoxYesNo(string msg, string title = STOCK_TITLE) {
+			var result = MessageBox.Show(msg, title, MessageBoxButton.YesNo);
+			return (result == MessageBoxResult.Yes);
+		}
+
+		public static string SelectFileWithWin32OpenDialog(string filter) {
+			var openFileDialog = new OpenFileDialog();
+			if (!string.IsNullOrWhiteSpace(filter)) {
+				openFileDialog.Filter = filter;
+			}
+			if (openFileDialog.ShowDialog() == true) {
+				return openFileDialog.FileName;
+			}
+			return string.Empty;
+		}
+
+		
+	}
+}
--- WaveSandbox/ViewModel/MainViewModel.cs
+++ WaveSandbox/ViewModel/MainViewModel.cs
@@ -1,4 +1,7 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Windows.Input;
+using WaveSandbox.Extensions;
 
 namespace WaveSandbox.ViewModel
 {
@@ -29,6 +32,38 @@ namespace WaveSandbox.ViewModel
             ////{
             ////    // Code runs "for real"
             ////}
+
+			this.SelectFile1Command = new RelayCommand<object>(p => SelectFile1(p));
+			this.SelectFile2Command = new RelayCommand<object>(p => SelectFile2(p));
+			this.CombineFileCommand = new RelayCommand<object>(p => CombineFiles(p));
         }
+
+		public ICommand SelectFile1Command {
+			get;
+			private set;
+		}
+
+		public ICommand SelectFile2Command {
+			get;
+			private set;
+		}
+
+		public ICommand CombineFileCommand {
+			get;
+			private set;
+		}
+
+		public void SelectFile1(object o) {
+			UIServices.ShowMessageBox("Coming Soon!", "Foo");
+		}
+
+		public void SelectFile2(object o) {
+
+		}
+
+		public void CombineFiles(object o) {
+
+		}
+
     }
 }
